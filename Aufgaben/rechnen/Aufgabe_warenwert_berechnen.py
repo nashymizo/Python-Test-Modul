46b816ebdceef5e77bc6a896700fbefe4ddcb017
nashymizo <nico.kehl92@web.de>
2026-07-21 14:39:46 +0200

    Aufgabe_warenwert_berechnen.py - erledigt

--- Aufgaben/rechnen/Aufgabe_warenwert_berechnen.py
+++ Aufgaben/rechnen/Aufgabe_warenwert_berechnen.py
@@ -23,3 +23,37 @@ Programmieraufgabe (Rabatte & Zusatzkosten beim Online-Einkauf)
 	- den Endpreis nach allen Abzügen und Zuschlägen.
 	
 """
+
+def Kassenbon(warenwert):
+
+    warenwert = float(warenwert)
+
+    mengenrabatt = warenwert * 0.1
+    sonderrabatt = warenwert * 0.05
+    versandkosten = 4.90
+    verpackungskosten = warenwert * 0.02
+    mehrwertsteuer = 0.19
+
+    zwischensumme = warenwert - mengenrabatt - sonderrabatt + versandkosten + verpackungskosten
+
+    berechnete_mehrwertsteuer = zwischensumme * mehrwertsteuer
+
+    endsumme = zwischensumme + berechnete_mehrwertsteuer
+
+    return warenwert, mengenrabatt, sonderrabatt, versandkosten, verpackungskosten, berechnete_mehrwertsteuer, endsumme
+
+
+# MÖGLICHKEIT OHNE TUPEL
+# 1. Wir rufen die Funktion auf und fangen ALLE 7 Rückgabewerte der Reihe nach auf:
+# wert, m_rabatt, s_rabatt, v_kosten, verp_kosten, steuer, summe = Kassenbon(200)
+
+mein_bon = Kassenbon(200)
+
+print(f"----- IHR EINKAUF -----")
+print(f"{'Einkauf:':<26} {mein_bon[0]:.2f} €")
+print(f"{'Mengenrabatt:':<26} -{mein_bon[1]:.2f} €")
+print(f"{'Sonderrabatt:':<26} -{mein_bon[2]:.2f} €")
+print(f"{'Versandkosten:':<26} +{mein_bon[3]:.2f} €")
+print(f"{'Verpackungskosten:':<26} +{mein_bon[4]:.2f} €")
+print(f"{'Mehrwertsteuer:':<26} +{mein_bon[5]:.2f} €")
+print(f"{'ENDBETRAG:':<24} = {mein_bon[6]:.2f} €")
