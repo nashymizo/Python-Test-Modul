50ca807ae39e1d0e4d1eef0d8458b6f0a7597a04
nashymizo <nico.kehl92@web.de>
2026-07-23 23:56:24 +0200

    array vervollständigt

--- 16072026.py
+++ 16072026.py
@@ -16,4 +16,26 @@ print(re.findall(r"[A-Z][0-9]+", text))
 print(re.findall(r" ([0-9+]+)", text))
 
 
+def einfacher_decorator(funktion):
+    def wrapper(*args, **kwargs):
+        ergebnis = funktion(*args, **kwargs)
 
+        if "RiinaDuck" in args:
+            print(r"""
+   __
+<(o )___
+( ._> /
+  `---'
+""")
+
+        return ergebnis
+
+    return wrapper
+
+
+@einfacher_decorator
+def sage_hallo(name):
+    return f"Hallo {name}"
+
+
+print(sage_hallo("RiinaDuck"))
